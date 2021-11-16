9ed778902eee9c5a418c7dfa6afcd0930d541fbe
João <joao.pedro.fernandes.122@gmail.com>
2021-11-16 12:16:47 -0300

    atribuição

---
+++ Conceitos iniciais/5-atribuicao.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com atribuição:");
+
+const idade = 20;
+const nome = "João";
+const  sobrenome = "Fernandes";
+
+console.log(nome,sobrenome)
+//console.log(nome + " " + sobrenome)
+
+console.log(`Meu nome é ${nome} ${sobrenome}`)
