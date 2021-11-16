2ef79ea996ff8b55ba231e571f05eb97ce5bc5db
João <joao.pedro.fernandes.122@gmail.com>
2021-11-16 13:55:26 -0300

    trabalhando com listas

---
+++ Conceitos iniciais/6-listas.js
@@ -0,0 +1,14 @@
+console.log(`trabalhando com listas`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+    );
+
+
+listaDeDestinos.push(`Curitiba`);
+listaDeDestinos.push(`Belo Horizonte`);
+listaDeDestinos.splice(1,1)
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[1]);
